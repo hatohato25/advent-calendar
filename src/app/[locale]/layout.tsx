175b6a30b878186f95a5e939340808929ae83dee
hatohato25 <hatohato25@gmail.com>
2026-02-28 18:39:12 +0900

    mod feat: next の Analytics を追加しました

--- src/app/[locale]/layout.tsx
+++ src/app/[locale]/layout.tsx
@@ -1,3 +1,4 @@
+import { Analytics } from "@vercel/analytics/next";
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import { notFound } from "next/navigation";
@@ -62,6 +63,7 @@ export default async function LocaleLayout({
           </ThemeProvider>
         </SessionProvider>
         <GoogleAnalytics />
+        <Analytics />
       </body>
     </html>
   );
